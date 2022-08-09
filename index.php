da596ab9d8e141a39ffec91863e4ea9aa605c9f5
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-08-08 19:21:51 -0600

    Archivo PHP

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+
+    <div class="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="tablas.html">Tablas</a></li>
+            <li><a href="formularios.html">Formularios</a></li>
+        </ul>
+    </div>
+
+    <div class="division">
+        <div class="izquierdo">
+            <div class="img">
+                <figure>
+                    <img src="/src/laptop3.jpg" alt="Imagen de laptop" class="dimensionImg">
+                
+                </figure>
+            </div>
+
+        </div>
+
+        <div class="derecho">
+            <p class="titulo">BIENVENIDO</p>
+            <P>Al examen parcial de desarrollo Web, en las paginas que usted verá
+                aquí podrá ver el avance que hemos tenido en las primeras semanas de clase
+            </P>
+
+
+        </div>
+    </div>
+
+    <div class="piePag">
+        <h2>Todos los derechos reservados a /UMG/</h2>
+    </div>
+
+
+</body>
+
+</html>
